1fa58c9c53d0a37009295301dee811f36e51bd21
Adrian Wennberg <saikoupanda@gmail.com>
2017-04-02 09:00:25 +0100

    Added some general functions and did some cleaning.

---
+++ generalFunctions.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Assignment3Header.h"
+
+int RandInt(int min, int max)
+{
+    max -= min;
+    int upperBound = RAND_MAX - RAND_MAX % max;
+    int randInt;
+
+    do
+    {
+        randInt = rand();
+    }while(randInt >= upperBound);
+
+    randInt %= max + 1;
+    
+    randInt += min;
+
+    return randInt;
+}
+
+void UserInput(int maxLength, char *returnString)
+{
+    char *tempInput = malloc((maxLength + 1));
+    do
+    {
+        printf("\n : ");
+        
+        tempInput[maxLength - 1] = '\0';
+        fgets(tempInput, maxLength + 1, stdin);
+        
+        int i = 0;
+        while(tempInput[i] != '\n' && tempInput[++i]);
+        
+        if(!tempInput[i])
+        {
+            while(getchar() != '\n');
+            printf("Input must be less than %d characters.", maxLength);
+        }
+        else
+        {
+            tempInput[i] = '\0';
+        }
+    }while(tempInput[maxLength - 1]);
+    
+    strcpy(returnString, tempInput);
+}
+
+
+int NumberInput(int maxDigits)
+{
+    char *tempInput = malloc(maxDigits);
+    char *endPtr;
+    int returnNumber;
+    
+    do
+    {
+        UserInput(maxDigits, tempInput);
+    } while(!(returnNumber = strtol(tempInput, &endPtr, 10)) || *endPtr);
+    
+    return returnNumber;
+}
