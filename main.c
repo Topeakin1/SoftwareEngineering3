3d59a10c79bf54a5583ae55b74b2b63c439d6f45
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-04-03 12:52:21 +0100

    started the main

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include "Assignment3.h"
+
+int main(void) {
+	srand(time(NULL));
+	Player players[PLAYER_MAX]; 
+    Slot slots[SLOT_MAX];
+    
+	int slotsCount;
+	int playersCount;    
+}
+	
