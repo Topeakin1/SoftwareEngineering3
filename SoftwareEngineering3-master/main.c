31c2d83a0a1135a47b1aff3c60fd6f54b6ad43d3
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-04-03 11:45:36 +0100

    changes

---
+++ SoftwareEngineering3-master/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "Assignment3Header.h"
+
+int main(void) {
+	srand(time(NULL));
+	Player players[PLAYER_MAX]; 
+    Slot slots[SLOT_MAX];
+    
+    int slotsCount;
+	int playersCount;    
+    
+}
