693c229eaba58d41480fbe8d99c7950ad6272af8
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-04-11 09:37:51 +0100

    called all the functions in the main
    we might still need to modify a few of them to suit this project but i
    called them in the main so they can be used

--- main.c
+++ main.c
@@ -12,6 +12,11 @@ int main(void) {
 	
 	InputPlayerInfo(playersCount, players);
 	NumberOfPlayers();
+	Print(players, numberOfPlayers);
+	movePlayer(player,slots, currentPosition, newPosition);
+	PlayerAction( playerCount, slotCount, players, slots);
+	PlayersInSlots(player);
+	attackPlayer(attacker, attacked);
 	   
 	while (alivePlayers>=2) {
 		alivePlayers= PlayersCount;
@@ -19,7 +24,7 @@ int main(void) {
 		  alivePlayers--;
 		  printf("The %s is dead \n",players[i]);
 	}
-	GameRound(int alivePlayers, Player);//function call
+	GameRound( alivePlayers, Player);//function call
 	     
 	}
 		 
